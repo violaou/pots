178863a3e47b50688d3c37e1e833601e70ba370b
viola <viola@vee.local>
2025-08-17 22:47:36 -0400

    Add VITE_FILLOUT_ID to environment template and update Contact component to use dynamic Fillout ID

--- src/pages/Contact.tsx
+++ src/pages/Contact.tsx
@@ -1,11 +1,10 @@
 import '@fillout/react/style.css'
 import { FilloutStandardEmbed } from '@fillout/react'
 
+const filloutId = import.meta.env.VITE_FILLOUT_ID as string
+
 export const Contact = () => (
-  <>
-    <div> Hi</div>
-    <section className=" md:mb-16 h-[600px] lg:h-[700px]">
-      <FilloutStandardEmbed filloutId="v1DC683uyyus" />
-    </section>
-  </>
+  <section className=" md:mb-16 h-[600px] lg:h-[700px]">
+    <FilloutStandardEmbed filloutId={filloutId} />
+  </section>
 )
