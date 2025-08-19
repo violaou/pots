533987615a29f947fcbee19c659fcbe505f87b7c
viola <viola@vee.local>
2025-08-19 08:48:57 -0400

    analytics

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,8 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { Analytics } from '@vercel/analytics/react'
+import { SpeedInsights } from '@vercel/speed-insights/react'
+
 import App from './App'
 import './index.css'
 
@@ -9,5 +12,7 @@ if (!root) throw new Error('Root element not found')
 createRoot(root).render(
   <StrictMode>
     <App />
+    {import.meta.env.PROD && <Analytics />}
+    {import.meta.env.PROD && <SpeedInsights />}
   </StrictMode>
 )
